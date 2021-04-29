ea2111aa74de8f86bcc6b90bd1f92dd7ba17ce53
gustavoparedes1978 <gustavoparedes1978@gmail.com>
2021-04-29 14:47:25 -0500

    Update WebSocket.js

--- public/js/WebSocket.js
+++ public/js/WebSocket.js
@@ -185,7 +185,7 @@ function startWebSocket(socket,streamName)
     {
         console.log('socket state onopen '+socket.readyState);
         var message = "{\"method\": \"SUBSCRIBE\",\"params\": [\""+streamName+"\"],\"id\": \"1\" }";
-        if(socket.readyState===0){sockets.send(message);}
+        if(socket.readyState===0){socket.send(message);}
         lowestATR = sessionStorage.getItem('ATR_SMAs_Array');
         lastBreakPoint =  sessionStorage.getItem('lastBreakPoint');
         console.log(lowestATR+' '+lastBreakPoint);  
