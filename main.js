41e36f55cdf9ad5b7b85dbd0b6e7e95f18089c4e
gustavoparedes1978 <gustavoparedes1978@gmail.com>
2021-06-08 12:54:22 -0500

    log testing

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 
+module.exports.log = function (msg) { 
+    console.log('testing message');
+};
+
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     //console.log(q.pathname);
