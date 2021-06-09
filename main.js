d3b518b28f0edf20ffc0cd41878decefec52f581
gustavoparedes1978 <gustavoparedes1978@gmail.com>
2021-06-08 20:14:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -441,7 +441,7 @@ function startWebSocket(socket,streamName)
 
 function pingFunction()
 {
-    var hosts = ['radiant-headland-62259.herokuapp.com/'];
+    var hosts = ['radiant-headland-62259.herokuapp.com'];
     hosts.forEach(function(host){
         ping.sys.probe(host, function(isAlive){
         var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
