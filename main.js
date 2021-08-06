9951fff26ac45aee2a19cab2cc567c11f676bb0f
gustavoparedes1978 <gustavoparedes1978@gmail.com>
2021-08-06 13:11:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ const client = new Client({
 client.connect();
 
 client.query('INSERT INTO ORDERS (BALANCE,OPENPRICE,CLOSEPRICE,ORDERTYPE,STATUS) VALUES (20000.00, 5000.00, 52000.00, BUY, OPEN); ', (err, res) => {
-    if (err) throw err;
     client.end();
 });
 
